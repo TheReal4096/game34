11873a3eccdc4c2774099c49210b4efd83dc3855
TheReal2048 <69132192+TheReal4096@users.noreply.github.com>
2020-08-31 21:04:11 -0700

    Update js/5x5/game_manager.js

--- js/5x5/game_manager.js
+++ js/5x5/game_manager.js
@@ -72,7 +72,7 @@ GameManager.prototype.addStartTiles = function () {
 // Adds a tile in a random position
 GameManager.prototype.addRandomTile = function () {
   if (this.grid.cellsAvailable()) {
-    var value = 1;
+    var value = Math.random() < 0.5 ? 1597 : 2584;
     var tile = new Tile(this.grid.randomAvailableCell(), value);
 
     this.grid.insertTile(tile);
